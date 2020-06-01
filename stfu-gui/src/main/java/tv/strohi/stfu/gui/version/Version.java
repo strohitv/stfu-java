5b2c9c224d0b058b6857be86d6ebca1813892974
marco <goethe@marco-fuchs.de>
2020-06-01 21:25:38 +0200

    reset version to 1.0.0-SNAPSHOT

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/version/Version.java
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/version/Version.java
@@ -2,7 +2,7 @@ package tv.strohi.stfu.gui.version;
 
 public final class Version {
 
-    private static final String VERSION = "1.5.0-SNAPSHOT";
+    private static final String VERSION = "1.0.0-SNAPSHOT";
     private static final String GROUPID = "tv.strohi.stfu";
     private static final String ARTIFACTID = "stfu-gui";
     private static final String REVISION = "${buildNumber}";
