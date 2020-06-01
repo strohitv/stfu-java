25fab9bbb647ab697314ed86545679303d354e25
marco <goethe@marco-fuchs.de>
2020-06-01 12:33:34 +0200

    style the gui for a maximized layout

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/App.java
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("windows/main"));
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
