b642ef138b443e93d9c6fb255321c74076753f52
marco <goethe@marco-fuchs.de>
2020-06-06 19:39:16 +0200

    add translated items to sort order combobox

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/PathsController.java
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/PathsController.java
@@ -2,6 +2,7 @@ package tv.strohi.stfu.gui.scenes;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 import tv.strohi.stfu.gui.i18n.LocalizationBinder;
 
@@ -10,10 +11,21 @@ import java.util.ResourceBundle;
 
 public class PathsController implements Initializable {
     @FXML
-    public GridPane mainPane;
+    private GridPane mainPane;
+
+    @FXML
+    private ComboBox<String> sortOrderComboBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         LocalizationBinder.addListeners(mainPane.getChildren().toArray(),"bundles.scenes.paths");
+
+        LocalizationBinder.addComboboxListener(sortOrderComboBox, "bundles.scenes.paths",
+                "edit.sortorder.nameasc", "edit.sortorder.namedsc",
+                "edit.sortorder.creationdateasc", "edit.sortorder.creationdatedsc",
+                "edit.sortorder.changedateasc", "edit.sortorder.changedatedsc",
+                "edit.sortorder.sizeasc", "edit.sortorder.sizedsc");
+
+        sortOrderComboBox.getSelectionModel().select(0);
     }
 }
