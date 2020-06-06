aaf745831538970bde02a19951f1571dee9a71fe
marco <goethe@marco-fuchs.de>
2020-06-06 16:45:00 +0200

    remove unused language controller

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/LanguageController.java
+++
@@ -1,9 +0,0 @@
-package tv.strohi.stfu.gui.scenes;
-
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public abstract class LanguageController implements Initializable {
-}
