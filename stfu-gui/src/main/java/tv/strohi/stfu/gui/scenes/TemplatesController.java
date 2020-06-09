45ef19c810894f600c75a45be2859b05cd0f850d
marco <goethe@marco-fuchs.de>
2020-06-09 10:10:31 +0200

    implement first two tabs of templates stage

---
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/TemplatesController.java
@@ -0,0 +1,22 @@
+package tv.strohi.stfu.gui.scenes;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.SplitPane;
+import javafx.stage.Screen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TemplatesController implements Initializable {
+    @FXML
+    private SplitPane mainPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        mainPane.setPrefHeight(primaryScreenBounds.getHeight() * 0.8);
+        mainPane.setPrefWidth(primaryScreenBounds.getWidth() * 0.8);
+    }
+}
