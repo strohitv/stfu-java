6df83b4deed491410ceadd83a1701b70af07037e
marco <goethe@marco-fuchs.de>
2020-06-10 19:34:24 +0200

    implemented translations. english needs to be done.

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/TemplatesController.java
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/TemplatesController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.SplitPane;
 import javafx.stage.Screen;
+import tv.strohi.stfu.gui.i18n.LocalizationBinder;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +16,8 @@ public class TemplatesController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        LocalizationBinder.addListeners(mainPane.getItems().toArray(),"bundles.scenes.templates");
+
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         mainPane.setPrefHeight(primaryScreenBounds.getHeight() * 0.8);
         mainPane.setPrefWidth(primaryScreenBounds.getWidth() * 0.8);
