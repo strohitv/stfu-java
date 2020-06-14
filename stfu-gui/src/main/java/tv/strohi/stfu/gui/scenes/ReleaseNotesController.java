d3d0b9ff03ef5ba809e3e6b12d1c8fa3aa772776
marco <goethe@marco-fuchs.de>
2020-06-15 01:38:53 +0200

    add release notes. They still need to be translated.

--- stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/ReleaseNotesController.java
+++ stfu-gui/src/main/java/tv/strohi/stfu/gui/scenes/ReleaseNotesController.java
@@ -3,6 +3,8 @@ package tv.strohi.stfu.gui.scenes;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebView;
+import tv.strohi.stfu.gui.App;
 import tv.strohi.stfu.gui.i18n.LocalizationBinder;
 
 import java.net.URL;
@@ -12,8 +14,17 @@ public class ReleaseNotesController implements Initializable {
     @FXML
     private GridPane mainPane;
 
+    @FXML
+    private WebView releaseNotes;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         LocalizationBinder.addListeners(mainPane.getChildren().toArray(), "bundles.scenes.releasenotes");
+
+        releaseNotes.getEngine().setJavaScriptEnabled(true);
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+
+        URL url = getClass().getResource("/releasenotes/releasenotes_" + App.getLocale().getLanguage() + ".html");
+        releaseNotes.getEngine().load(url.toString());
     }
 }
